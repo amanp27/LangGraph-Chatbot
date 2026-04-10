bfea1a5278d31143bec45cd4bc14a7ff4ace2a5a
Aman Prajapati <amanprajapati22@gmail.com>
2026-04-10 18:21:32 +0530

    threading added

--- streamlit_threading.py
+++ streamlit_threading.py
@@ -1,13 +1,38 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
+
 from dotenv import load_dotenv
 load_dotenv()
 
-CONFIG = {'configurable': {'thread_id': '1'}}
+# UTILS ----------------------------------------------------------------
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+
+# SESSION STATE ---------------------------------------------------------
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+
+# SIDEBAR ---------------------------------------------------------------
+
+st.sidebar.title("LangGraph Chatbot")
+st.sidebar.button("New Chat")
+st.sidebar.header("Chat History")
+
+st.sidebar.text(st.session_state['thread_id'])
+
+
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
